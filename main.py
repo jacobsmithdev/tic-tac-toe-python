d6c23f86f7bcab52fb495a503e6327f466bd405f
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-12-30 11:48:11 -0700

    style: Format code based on Black Formatter

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ BOARD_SIZE = 3
 # format board as [rows][cols]
 gameboard = [[None] * BOARD_SIZE for n in range(BOARD_SIZE)]
 
+
 def log_board():
-    print('Gameboard')
-    print('┌───┬───┬───┐')
+    print("Gameboard")
+    print("┌───┬───┬───┐")
     for row_index, row in enumerate(gameboard):
 
-        print('│', end='')
+        print("│", end="")
         for cell in row:
             if cell == None:
-                cell = ' '
-            print(f' {cell} ', end='')
-            print('│', end='')
-
-
+                cell = " "
+            print(f" {cell} ", end="")
+            print("│", end="")
 
         print()
         if row_index == len(gameboard) - 1:
-            print('└───┴───┴───┘')
+            print("└───┴───┴───┘")
         else:
-            print('├───┼───┼───┤')
+            print("├───┼───┼───┤")
 
 
 def place_character(row, col, char):
@@ -30,13 +29,13 @@ def place_character(row, col, char):
 
     if not valid_coords(row, col):
         return False
-    
+
     if gameboard[row][col]:
         return False
 
     gameboard[row][col] = char
     return True
-        
+
 
 def valid_coords(row, col):
     if 0 < row > BOARD_SIZE - 1:
