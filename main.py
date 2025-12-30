5b2056a32b7b524e3304b60fb4cfc5bdeaa150e5
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-12-30 11:14:34 -0700

    feat: Add function to log board to console

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ gameboard = [
     [None, None, None],
     [None, None, None],
 ]
+
+def log_board():
+    print('Gameboard')
+    print('┌───┬───┬───┐')
+    for row_index, row in enumerate(gameboard):
+
+        print('│', end='')
+        for cell in row:
+            if cell == None:
+                cell = ' '
+            print(f' {cell} ', end='')
+            print('│', end='')
+
+
+
+        print()
+        if row_index == len(gameboard) - 1:
+            print('└───┴───┴───┘')
+        else:
+            print('├───┼───┼───┤')
